94c14ac1eaa43d2d6012055665df3d043f2a0f0c
Andrey Santos <andrey.barros.santos@gmail.com>
2017-03-07 14:01:50 -0300

    Bump Version Spring Boot

--- sales-control-api/src/main/java/br/com/sinergiavirtual/sc/api/ModelConfig.java
+++ sales-control-api/src/main/java/br/com/sinergiavirtual/sc/api/ModelConfig.java
@@ -2,18 +2,18 @@ package br.com.sinergiavirtual.sc.api;
 
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 public class ModelConfig {
-        @Bean
-        ServletRegistrationBean h2servletRegistration(){
-            ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
-            registrationBean.addUrlMappings("/console/*");
-            return registrationBean;
-        }
-    }
 
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
--- sales-control-api/src/test/java/br/com/sinergiavirtual/sc/SalesControlApplicationTests.java
+++ sales-control-api/src/test/java/br/com/sinergiavirtual/sc/SalesControlApplicationTests.java
@@ -3,12 +3,11 @@ package br.com.sinergiavirtual.sc;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SCApiApplication.class)
+//@SpringApplicationConfiguration(classes = SCApiApplication.class)
 @WebAppConfiguration
 @Ignore
 public class SalesControlApplicationTests {
